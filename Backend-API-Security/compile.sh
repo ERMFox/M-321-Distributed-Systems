5454b4fcba7208badeadae19978b0cca79ead043
ERMFox <119940578+ERMFox@users.noreply.github.com>
2024-11-18 15:03:18 +0100

    Delete Backend-API-Security/compile.sh

--- Backend-API-Security/compile.sh
+++
@@ -1,9 +0,0 @@
-g++ -std=c++17 src/main.cpp src/handlers/jwt_handler.cpp src/handlers/html_handler.cpp -o server \
-    -I src/include \
-    -I /usr/local/include \
-    -L /usr/local/lib \
-    -lpistache \
-    -lssl \
-    -lcrypto \
-    -lpthread \
-    -ljwt
